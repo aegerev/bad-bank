836976389276f1296e4b0165b7993d7dca2ff7ab
alex <alexandersegerev@gmail.com>
2024-09-10 10:59:48 -0400

    Added all the necessary componenets

--- bad-bank/src/index.js
+++ bad-bank/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; 
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Navbar from 'react-bootstrap';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App.js'
+import CreateAccount from './components/createaccount.js'
+import Login from './components/login.js'
+import Deposit from './components/deposit.js'
+import Withdraw from './components/withdraw.js'
+import Balance from './components/balance.js'
+import AllData from './components/alldata.js'
+
+function Spa() {
+  return (
+    <HashRouter>
+      <NavBar/>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+        <div className="container" style={{padding: "20px"}}>
+          <Route path="/" exact component={App} />
+          <Route path="/CreateAccount/" component={CreateAccount} />
+          <Route path="/login/" component={Login} />
+          <Route path="/deposit/" component={Deposit} />
+          <Route path="/withdraw/" component={Withdraw} />
+          <Route path="/balance/" component={Balance} />
+          <Route path="/alldata/" component={AllData} />
+        </div>
+      </UserContext.Provider>      
+    </HashRouter>
+  );
+}
+
+ReactDOM.render(
+  <Spa/>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
